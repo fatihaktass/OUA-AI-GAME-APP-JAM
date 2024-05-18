f0b1aa3bd5d5ed4dc92badffae88e314bcdf4e30
Muhammet Fatih Aktaş <faatihaktaas@gmail.com>
2024-05-19 01:08:09 +0300

    karakter animasyonları

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Transform groundChecker;
     [SerializeField] bool isCrouch;
 
+    [SerializeField] GameObject player;
+    [SerializeField] Animator playerAnimator;
+
     float horizontalMovement;
     float verticalMovement;
     Vector3 gravityV3;
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         charCont = GetComponent<CharacterController>();
+        playerAnimator = player.GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -38,10 +42,15 @@ public class PlayerController : MonoBehaviour
         Vector3 V3movement = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
         charCont.Move(moveSpeed * Time.deltaTime * V3movement);
 
+        playerAnimator.SetFloat("Horizontal", horizontalMovement);
+        playerAnimator.SetFloat("Vertical", verticalMovement);
+
         //Jump ------------------------------------------------------------------------------------------------------------- //
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             gravityV3.y = Mathf.Sqrt(jumpForce * -2f * gravityForce);
+            playerAnimator.ResetTrigger("FinishedJump");
+            playerAnimator.SetTrigger("Jumping");
         }
 
         //Crouch ----------------------------------------------------------------------------------------------------------- //
@@ -66,9 +75,11 @@ public class PlayerController : MonoBehaviour
             if (horizontalMovement != 0 || verticalMovement != 0)
             {
                 moveSpeed = 5f;
+                playerAnimator.SetFloat("IsRunning", 0);
                 if (Input.GetKey(KeyCode.LeftShift))
                 {
                     moveSpeed = 8f;
+                    playerAnimator.SetFloat("IsRunning", 1);
                 }
             }
             else
@@ -88,6 +99,7 @@ public class PlayerController : MonoBehaviour
         if (gravityV3.y < 0 && isGrounded)
         {
             gravityV3.y = -3f;
+            playerAnimator.SetTrigger("FinishedJump");
         }
     }
 }
